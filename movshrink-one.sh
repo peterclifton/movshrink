e6454794d32b31ee72970d2566f92cc3a8f06df8
Peter Clifton <peterclifton.j@gmail.com>
2025-06-06 00:53:50 +0100

    initial commit

---
+++ movshrink-one.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#--------------------------
+# Functions
+#--------------------------
+
+get_vid_length () {
+    # usage: get_vid_length somevideo.MOV
+    # e.g: ffprobe -i VID_20191205_002640.MOV -show_entries format=duration -v quiet -of csv="p=0"
+    echo "$(ffprobe -i $1 -show_entries format=duration -v quiet -of csv="p=0")"
+}
+
+#--------------------------
+# Main logic
+#--------------------------
+
+# sanity check to make sure an argument has been provided
+if [ -z "$1" ]; then
+    echo "::  expected one argument (string to pass to append as line to master_learn.md)"
+    exit 1
+fi
+
+# append the arg1 string to the target file
+input_mov_file=$1
+MP4filename=$(echo ${input_mov_file} | sed -e 's/MOV$/mp4/')
+
+logfile=".movshrinker"$(date +%s)".txt"
+touch $logfile
+
+# https://stackoverflow.com/questions/47115191/when-i-run-ffmpeg-in-the-background-how-do-i-prevent-suspended-tty-output
+# https://stackoverflow.com/questions/8220098/how-to-redirect-the-output-of-an-application-in-background-to-dev-null
+
+VID_LENGTH=$(get_vid_length ${input_mov_file})
+echo ":: Video length (seconds): ${VID_LENGTH}"
+
+ffmpeg -nostdin -loglevel quiet -progress ${logfile} -i ${input_mov_file} -c:v libx265 -crf 28 -preset medium -tag:v hvc1 -c:a aac -b:a 128k ${MP4filename} > /dev/null 2>&1 & XPID=$!
+
+echo ""
+let grep_result=1
+while ((grep_result>0)) && [ -e /proc/$XPID ]  ; do
+    # https://stackoverflow.com/questions/11283625/overwrite-last-line-on-terminal
+    echo -e "\r\033[1A\033[0K $(grep 'out_time=' ${logfile} | tail -n 1) $(grep speed ${logfile} | tail -n 1)"
+    grep 'progress=end' ${logfile} > /dev/null 2>&1
+    let grep_result=$?
+    sleep 5
+done
+
+grep 'progress=end' ${logfile} > /dev/null 2>&1
+let grep_result2=$?
+echo ":: Result: ${grep_result2}"
+rm $logfile
+exit $grep_result2
+
+
---
+++ movshrink.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+# usage example: movshrinker -t
+
+# --------------------
+# Functions
+# -------------------
+
+numbers () {
+    echo ""
+    echo "------------------------------------------------"
+    echo "$(echo -n 'MOV: ' ; ls | grep MOV | wc -l) ----- $(echo -n 'mp4: ' ; ls | grep mp4 | wc -l)"
+    echo "------------------------------------------------"
+}
+
+help () {
+    cat <<EOF
+:: This program will attempt to compress any .mov
+   file in the current working directory.
+   And delete the orignal.
+   Use at own risk!
+EOF
+}
+
+let response='N'
+get_response () {
+    # offer for user to exit before proceeding
+    cat <<EOF
+:: This program will attempt to compress any .mov
+   file in the current working directory.
+   And delete the orignal.
+   Use at own risk!
+
+   Current working directory is: ${PWD}
+
+   Enter Y to proceed or just press ENTER to exit safely
+EOF
+
+    read user_response
+    if [ ! -z "$user_response" ]; then
+        let response=$user_response
+    fi
+}
+
+
+check_before_proceeding () {
+    get_response
+
+    if [ ${#user_response} -eq 0 ]; then
+        echo "You choose to exit, exiting now..."
+        exit 0;
+    elif [ $user_response = "Y" ]; then
+        echo ""
+    else
+        echo "You choose to exit, exiting now..."
+        exit 0;
+    fi
+}
+
+# --------------------
+# Main logic
+# -------------------
+
+mode="N"
+limit_to_10="n"
+
+# expects -h or -v as options in $1 or else no options
+
+if [ -n "$1" ]; then  # i.e. an command line argument has been provided
+    case "$1" in
+        -h) mode="H" ;; # will just print a help string
+        -t) mode="T" ;; # verbose mode
+        *) echo "::Option $1 not recognised, exiting";exit 1;
+    esac
+fi
+
+#echo "MODE=${mode}"
+
+if  [ $mode = "H" ]; then # help mode
+    help
+    exit 0
+
+elif [ $mode = "N" ]; then # normal (default mode) 
+    :
+    #echo "Normal"
+
+elif [ $mode = "T" ]; then # verbose mode
+    #echo "Verbose"
+    limit_to_10="Y"
+fi
+
+check_before_proceeding
+
+let counter=0
+for FILE in *; do
+
+    if [ $mode = "T" ]; then
+        if [ $counter -gt 5 ]; then
+            echo ":: completed 5 iterations, exiting as planned..."
+            exit 0
+        fi
+    fi
+
+    if [[ $FILE == *.MOV ]]; then
+        MP4filename=$(echo ${FILE} | sed -e 's/MOV$/mp4/')
+        if [ -f "${MP4filename}" ]; then
+            echo ":: skipping as mp4 file of same name exists (${FILE})"
+        else
+            numbers
+            echo ":: compressing ${FILE}..."
+            let counter=counter+1
+
+            ./mvswatchgo.sh ${FILE}
+            
+            if [ $? -eq 0 ]; then
+                if [ -f "${MP4filename}" ]; then
+                    echo ":: deleting original ${FILE}"
+                    rm $FILE
+                fi
+            fi
+        fi
+        
+    else
+        echo ":: skipping non-mov file (${FILE})"
+    fi
+done
+
+exit 0
